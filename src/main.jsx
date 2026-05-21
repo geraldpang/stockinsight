c29aa3507a6f8fd2e9124fac9b2435f006d70769
Panggie <gpang77@gmail.com>
2026-05-22 08:16:02 +1000

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,22 @@
+import { useState, useEffect } from 'react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import { JournalPage } from './App.jsx'
+
+function Root() {
+  var [hash, setHash] = useState(window.location.hash);
+  useEffect(function() {
+    function onHash() { setHash(window.location.hash); }
+    window.addEventListener("hashchange", onHash);
+    return function() { window.removeEventListener("hashchange", onHash); };
+  }, []);
+  if (hash === "#journal") return <JournalPage />;
+  return <App />;
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Root />
   </StrictMode>,
 )
